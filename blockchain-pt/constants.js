8b214d832622caca0564499fcc18ba0ebaca1c30
Tanja Schmidt <tanjadev04@gmail.com>
2023-01-22 12:59:47 +0100

    add blockchain part incl. goerli deployment

---
+++ blockchain-pt/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x4e434B31406759fc44EdA1A6c5179771665e22D9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
